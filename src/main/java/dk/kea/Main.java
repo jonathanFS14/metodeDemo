76854740ae5f9d5c8992b441ec228b4b11639b65
jonathanFS14 <jona08p8@stud.kea.dk>
2022-09-12 09:18:18 +0200

    tilføjet person klasse med konstruktør

---
+++ src/main/java/dk/kea/Main.java
@@ -0,0 +1,9 @@
+package dk.kea;
+public class Main {
+    public static void main(String[] args) {
+
+    Person p1 = new Person(1.82);
+
+
+    }
+}
---
+++ src/main/java/dk/kea/Person.java
@@ -0,0 +1,10 @@
+package dk.kea;
+public class Person {
+
+    private double height;
+
+    public Person (double height){
+    this.height = height;
+    }
+
+}
