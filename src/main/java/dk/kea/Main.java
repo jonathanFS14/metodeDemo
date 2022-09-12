3e12a25fefb6cf9d600e72b0727520f0f510bf5d
jonathanFS14 <jona08p8@stud.kea.dk>
2022-09-12 10:10:16 +0200

    get højde og set højde

--- src/main/java/dk/kea/Main.java
+++ src/main/java/dk/kea/Main.java
@@ -3,7 +3,10 @@ public class Main {
     public static void main(String[] args) {
 
     Person p1 = new Person(1.82);
-
+    Person p2 = new Person(1.44);
+        System.out.println(p1.getHeight());
+    p1.setHeight(1.87);
+        System.out.println(p1.getHeight());
 
     }
 }
--- src/main/java/dk/kea/Person.java
+++ src/main/java/dk/kea/Person.java
@@ -2,9 +2,19 @@ package dk.kea;
 public class Person {
 
     private double height;
+    private int age;
 
     public Person (double height){
     this.height = height;
     }
+    public double getHeight() {
+        return height;
+    }
+    public void setHeight(double height){
+    this.height = height;
+    }
 
+    public void hasBirthday(){
+
+    }
 }
